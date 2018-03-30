a2508c8af28dd0aa922b4d6e7de1bcee7d4b207d
1103-5 <1103-5@1103-5-PC>
2018-03-30 21:09:36 +0900

    채팅 오류 수정했는데 뭐지

--- src/controller/ChatController.java
+++ src/controller/ChatController.java
@@ -12,6 +12,7 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -39,6 +40,8 @@ class ChatRoom {
 	}
 }
 
+@RequestMapping("/chat")
+@Controller
 public class ChatController extends TextWebSocketHandler {
 
 	@Autowired
