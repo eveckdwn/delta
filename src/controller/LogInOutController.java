d7fdf721dbdc4cf695cef36121a185de722b6661
1103-04 <1103-04@1103-04-PC>
2018-04-04 21:33:40 +0900

    중복 로그인 방지 - cj

--- src/controller/LogInOutController.java
+++ src/controller/LogInOutController.java
@@ -2,8 +2,12 @@ package controller;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +24,9 @@ import service.UsersService;
 @Controller
 @RequestMapping("/login")
 public class LogInOutController {
+	@Autowired
+	ServletContext application;
+	
 	@Autowired
 	GreetService greetService;
 
@@ -41,19 +48,50 @@ public class LogInOutController {
 		switch (result) {
 		case 0:
 			Map info = memberService.mypageInfo(map);
+			info.put("session", session.getId());
 			session.setAttribute("logon", info.get("ID"));
 			session.setAttribute("logonNick", info.get("NICK"));
 			BigDecimal bd = (BigDecimal)info.get("LV");
 			session.setAttribute("lv", bd.intValue());
+			
+			//	레벨에 따른 제한 알림 지우기
 			if(session.getAttribute("authlv") != null) {
 				session.removeAttribute("authlv");
 			}
+			
+			//	중복 로그인 처리
+			if(application.getAttribute("userList") == null){
+				List<Map> userList = new ArrayList();
+				userList.add(info);
+				application.setAttribute("userList", userList);	
+			}else{
+				List<Map> userList = (List<Map>) application.getAttribute("userList");
+				Iterator<Map> iter = userList.iterator();
+				while(iter.hasNext()){
+					Map i = iter.next();
+					if( info.get("ID").equals( i.get("ID") ) && !( info.get("session").equals( i.get("session") ) ) ){
+						
+						if(application.getAttribute((String) i.get("session")) != null){
+							application.setAttribute((String) i.get("session"), false);	
+						}
+						
+						iter.remove();
+						
+					}
+				}
+				userList.add(info);
+				application.setAttribute("userList", userList);
+			}
+			application.setAttribute((String) info.get("session"), true);
+			
+			//	페이지 관리자 체크
 			if(bd.intValue()==3) {
 				session.setAttribute("auth", true);
 				return "redirect:/admin";
 			}else {
 				return "redirect:/";
 			}
+			
 		case 1:
 			model.addAttribute("ment", greetService.make());
 			model.addAttribute("err", "회원 정보가 존재하지 않습니다.<br/>회원가입을 해주세요.");
@@ -78,6 +116,15 @@ public class LogInOutController {
 
 	@RequestMapping("/out")
 	public String logoutHandle(HttpSession session, Model model) {
+		List<Map> userList = (List<Map>) application.getAttribute("userList");
+		Iterator<Map> iter = userList.iterator();
+		while (iter.hasNext()) {
+			Map u = iter.next();
+			if ((u.get("session").equals(session.getId()))) {
+				application.removeAttribute((String) u.get("session"));
+				iter.remove();
+			}
+		}
 		session.removeAttribute("logon");
 		model.addAttribute("ment", greetService.make());
 		return "index";
